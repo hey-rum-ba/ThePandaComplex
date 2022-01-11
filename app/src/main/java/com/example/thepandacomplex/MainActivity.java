11b22ac66812d7cc5a57ebd9893d38200b174822
Heramba Panda <siddhartcool2000@gmail.com>
2022-01-11 21:36:20 +0530

    initial commit

--- app/src/main/java/com/example/thepandacomplex/MainActivity.java
+++ app/src/main/java/com/example/thepandacomplex/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.thepandacomplex;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
@@ -7,18 +9,24 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.thepandacomplex.databinding.ActivityMainBinding;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        ArrayList<MainModel> list =new ArrayList<>();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        ArrayList<MainModel> list = new ArrayList<>();
         list.add(new MainModel(0, 56, 60, 89, 56, "Madhu SK"));
         list.add(new MainModel(0, 55, 60, 89, 56, "Madhu SK"));
         list.add(new MainModel(0, 54, 60, 89, 56, "Madhu SK"));
@@ -32,10 +40,44 @@ public class MainActivity extends AppCompatActivity {
         list.add(new MainModel(0, 84, 60, 89, 56, "Madhu SK"));
         list.add(new MainModel(0, 59, 60, 89, 56, "Madhu SK"));
         list.add(new MainModel(0, 89, 60, 89, 56, "Madhu SK"));
-        MainAdapter adapter = new MainAdapter(list,this);
+        MainAdapter adapter = new MainAdapter(list, this);
         binding.renteesRecView.setAdapter(adapter);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         binding.renteesRecView.setLayoutManager(layoutManager);
+        ChildEventListener childEventListener = new ChildEventListener() {
+            private DataSnapshot snapshot;
+            @Nullable
+            private String previousChildName;
+
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                this.snapshot = snapshot;
+                this.previousChildName = previousChildName;
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+
+        };
     }
 
 
