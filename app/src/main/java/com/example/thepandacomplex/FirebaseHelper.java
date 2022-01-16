742e02941a7cd2b5afba52d0a9d7025f6f6f4bf5
Heramba Panda <siddhartcool2000@gmail.com>
2022-01-16 23:35:05 +0530

    initial commit

--- app/src/main/java/com/example/thepandacomplex/FirebaseHelper.java
+++ app/src/main/java/com/example/thepandacomplex/FirebaseHelper.java
@@ -1,5 +1,41 @@
 package com.example.thepandacomplex;
 
 public class FirebaseHelper {
+    int lastRent, electricity, noOfRentee ;
+    String name;
 
+    public FirebaseHelper(String name, int lastDate, int noOfRentee, int electricity) {
+    }
+
+    public int getLastRent() {
+        return lastRent;
+    }
+
+    public void setLastRent(int lastRent) {
+        this.lastRent = lastRent;
+    }
+
+    public int getElectricity() {
+        return electricity;
+    }
+
+    public void setElectricity(int electricity) {
+        this.electricity = electricity;
+    }
+
+    public int getNoOfRentee() {
+        return noOfRentee;
+    }
+
+    public void setNoOfRentee(int noOfRentee) {
+        this.noOfRentee = noOfRentee;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- app/src/main/java/com/example/thepandacomplex/addNewRentee.java
+++ app/src/main/java/com/example/thepandacomplex/addNewRentee.java
@@ -1,16 +1,30 @@
 package com.example.thepandacomplex;
 
-import android.os.Bundle;
+import static android.util.Log.*;
+import static androidx.constraintlayout.widget.ConstraintLayoutStates.TAG;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.thepandacomplex.databinding.AddNewRenteeBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.nio.file.ClosedFileSystemException;
 
 public class addNewRentee extends AppCompatActivity {
     AddNewRenteeBinding binding;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,8 +32,39 @@ public class addNewRentee extends AppCompatActivity {
         binding = AddNewRenteeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        DatabaseReference instance = FirebaseDatabase.getInstance().getReference("Rentee List");
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Rentee List 3");
 
+        binding.submit.setOnClickListener(view -> {
+            String name = binding.renteeNameInNewRentee.getText().toString();
+            int lastDate = binding.lastRentInNewRentee.getDayOfMonth();
+            int noOfRentee =  Integer.parseInt(binding.noOfRenteesInNewRentee.getText().toString()) ;
+            int electricity =  Integer.parseInt(binding.meterReading2InNewRentee.getText().toString()) -
+                    Integer.parseInt(binding.meterReading1InNewRentee.getText().toString());
+            d(TAG, "name :"+ name);
+            addDataToFireBase(name,lastDate,noOfRentee,electricity);
+        });
 
     }
+    private void addDataToFireBase(String name, int lastDate, int noOfRentee, int electricity){
+        d(TAG, "name is here too");
+        d(TAG, "name here is :"+name);
+        d(TAG, "name here is1 :"+lastDate);
+        d(TAG, "name here is2 :"+noOfRentee);
+        d(TAG, "name here is3 :"+electricity);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                FirebaseHelper helper= new FirebaseHelper(name,lastDate,noOfRentee,electricity);
+                databaseReference.setValue(helper);
+                Toast.makeText(addNewRentee.this, "Data added success", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(addNewRentee.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
+
